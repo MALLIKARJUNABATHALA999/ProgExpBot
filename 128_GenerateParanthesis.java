2280d80415905178fc9a6b088224207fdd1b21e4
MALLIKARJUNABATHALA999 <142199234+MALLIKARJUNABATHALA999@users.noreply.github.com>
2024-06-07 14:28:41 -0500

    Add files via upload

---
+++ 128_GenerateParanthesis.java
@@ -0,0 +1,20 @@
+class Solution {
+    List<String> result=new ArrayList<String>();
+    public List<String> generateParenthesis(int n) {
+        generatevalue(result,0,0,n,"");
+        return result;
+    }
+    private void generatevalue(List<String> result,int open,int close,int n,String str){
+        if(str.length()==2*n){
+            result.add(str);
+            return;
+        }
+        if(open<n){
+            generatevalue(result,open+1,close,n,str+"(");
+        }
+        if(close<open){
+            generatevalue(result,open,close+1,n,str+")");
+        }
+
+    }
+}
