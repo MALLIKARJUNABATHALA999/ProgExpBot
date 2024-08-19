fa0a7af7839d86cb68cea02f943d1f661098d15a
MALLIKARJUNABATHALA999 <142199234+MALLIKARJUNABATHALA999@users.noreply.github.com>
2024-08-19 11:41:24 -0500

    Add files via upload

---
+++ 209_Stack.java
@@ -0,0 +1,28 @@
+
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        if(top<arr.length-1){
+            top++;
+            arr[top]=x;
+        }
+    }
+
+    public int pop() {
+        if(top==-1){
+            return -1;
+        }
+        else{
+            int poppedElemnt=arr[top];
+            top--;
+            return poppedElemnt;
+        }
+    }
+}
